aebbc1296509ead7042cd0c0ecbdc1de0e242ae2
muhammadIhtishamKhan <ahtishamalamkhan@gmail.com>
2024-12-21 03:54:29 +0100

    feat: UndoManager

--- src/main/java/fr/istic/aco/editor/kernel/UndoManager.java
+++ src/main/java/fr/istic/aco/editor/kernel/UndoManager.java
@@ -1,18 +1,17 @@
 package fr.istic.aco.editor.kernel;
 
-import fr.istic.aco.editor.commands.Command;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class manages the undo, redo and store operations.
- *
  */
 public class UndoManager {
-    private final ArrayList<EditorSnapshot> pastStates;
-    private final ArrayList<EditorSnapshot> futureStates;
-    private final ArrayList<Pair<CommandOriginator, Memento>> pastCommands;
-    private final ArrayList<Pair<CommandOriginator, Memento>> futureCommands;
+    private final List<EditorSnapshot> pastStates;
+    private final List<EditorSnapshot> futureStates;
+    private final List<Pair<CommandOriginator, Memento>> pastCommands;
+    private final List<Pair<CommandOriginator, Memento>> futureCommands;
     int k = 5; //we store a snapshot after every k commands
     private final Engine engine;
 
@@ -33,7 +32,10 @@ public class UndoManager {
     public void storeCommand(CommandOriginator originator) {
         Memento memento = originator.generateMemento();
         pastCommands.add(new Pair<>(originator, memento));
-        futureCommands.clear();
+        int totalSize = this.pastCommands.size() + this.futureCommands.size();
+        if (totalSize == 1 || totalSize % k == 0) {
+            this.storeSnapshot(engine.createSnapshot());
+        }
     }
 
     /**
@@ -41,7 +43,6 @@ public class UndoManager {
      */
     public void storeSnapshot(EditorSnapshot snapshot) {
         pastStates.add(snapshot);
-        futureStates.clear();
     }
 
     /**
@@ -52,39 +53,59 @@ public class UndoManager {
     public void undo() {
         if (!pastCommands.isEmpty() && !pastStates.isEmpty()) {
             //Assign the past state to the current engine
-            EditorSnapshot pastState = pastStates.get(pastStates.size() - 1);
-            engine.restoreFrom(pastState);
-            //After restoring the state, we apply the last k commands
-            int i = 0;
-            while (i < k && !pastCommands.isEmpty()) {
-                //Execute the k commands since the last snapshot
-                Pair<CommandOriginator, Memento> command = pastCommands.get(pastCommands.size() - (k+i));
-                command.first().execute();
-                i++;
+            //if the snapshot was captured at the current stage, we go back to the previous state and recover all the steps from there
+            //if the snapshot was captured at the previous stage, we go back to the previous state and recover the last k steps from there
+            int loopSize;
 
+            //Check if the snapshot was captured at the current stage
+            if ((pastCommands.size() % k) == 0) {
+                //Remove the current state and add the current state to the future states
+                futureStates.add(pastStates.remove(getEndIndex(pastStates)));
+
+                //Assign the past state to the current engine
+                EditorSnapshot pastState = pastStates.get(getEndIndex(pastStates));
+                engine.restoreFrom(pastState);
+
+                loopSize = (getEndIndex(pastCommands) % k);
+
+            } else {
+                EditorSnapshot pastState = pastStates.get(getEndIndex(pastStates));
+                engine.restoreFrom(pastState);
+                loopSize = (pastCommands.size() % k);
             }
-            pastCommands.remove(pastCommands.size() - 1);
-            futureCommands.add(pastCommands.get(pastCommands.size() - 1));
+
+            for (int i = loopSize; i > 1; i--) {
+                Pair<CommandOriginator, Memento> pair = pastCommands.get(getEndIndex(pastCommands));
+                CommandOriginator command = pair.first();
+                Memento memento = pair.second();
+                command.restoreFrom(memento);
+                command.execute();
+                pastCommands.remove(getEndIndex(pastCommands));
+            }
+            pastCommands.remove(getEndIndex(pastCommands));
+            futureCommands.add(pastCommands.get(getEndIndex(pastCommands)));
         }
+    }
+
+    private int getEndIndex(List<?> list) {
+        return list.size() - 1;
     }
 
     public void redo() {
         if (!futureCommands.isEmpty() && !futureStates.isEmpty()) {
-            //Assign the future state to the current engine
-            EditorSnapshot futureState = futureStates.get(futureStates.size() - 1);
-            engine.restoreFrom(futureState);
-            //After restoring the state, we apply the last k commands
-            int i = 0;
-            while (i < k && !futureCommands.isEmpty()) {
-                Pair<CommandOriginator, Memento> command = futureCommands.get(futureCommands.size() - 1);
-                command.first().execute();
-                i++;
-            }
-            futureCommands.remove(futureCommands.size() - 1);
-            pastCommands.add(futureCommands.get(futureCommands.size() - 1));
+            //Execute the first command in the future commands
+            Pair<CommandOriginator, Memento> pair = futureCommands.get(0);
+            CommandOriginator command = pair.first();
+            Memento memento = pair.second();
+            command.restoreFrom(memento);
+            command.execute();
+            //Add the command to the past commands
+            pastCommands.add(futureCommands.get(0));
+            //Remove the command from the future commands
+            futureCommands.remove(0);
         }
     }
-
-
-
 }
+
+
+
